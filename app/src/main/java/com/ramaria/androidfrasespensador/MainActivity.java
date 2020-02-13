6f9183de3ca4aaa1896d338295e4b8ab15355942
RaquelMariah <45218570+RaquelMariah@users.noreply.github.com>
2020-02-13 14:35:11 -0300

    Adicionado array para frases

--- app/src/main/java/com/ramaria/androidfrasespensador/MainActivity.java
+++ app/src/main/java/com/ramaria/androidfrasespensador/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,8 +18,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gerarNovaFrase(View view){
+        String[] frases = {
+                "A arte de viver é simplesmente a arte de conviver... simplesmente, disse eu? Mas como é difícil!\n" +
+                        "\n" +
+                        "- Mario Quintana",
+                "A poesia não se entrega a quem a define.\n" +
+                        "\n" +
+                        "- Mario Quintana",
+                "E um dia os homens descobrirão que esses discos voadores estavam apenas estudando a vida dos insetos...\n" +
+                        "\n" +
+                        "- Mario Quintana\n",
+                "Maravilhas nunca faltaram ao mundo; o que sempre falta é a capacidade de senti-las e admirá-las.\n" +
+                        "\n" +
+                        "- Mario Quintana",
+                "O grande consolo das velhas anedotas são os recém-nascidos...\n" +
+                        "\n" +
+                        "- Mario Quintana",
+        };
+
+        int num = new Random().nextInt(5);
         TextView text = findViewById(R.id.textResult); // Localizando o elemento e setando na variavel
-        text.setText("Test success");
+        text.setText(frases[num]);
 
     }
 }
