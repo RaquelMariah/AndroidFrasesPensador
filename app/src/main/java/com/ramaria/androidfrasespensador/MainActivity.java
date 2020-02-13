6d3a3a3bfaa464b37d7f719c18d8c5601ee538f2
RaquelMariah <45218570+RaquelMariah@users.noreply.github.com>
2020-02-13 12:51:14 -0300

    Adicionado método gerarNovaFrase + evento do botão FraseDiaria

--- app/src/main/java/com/ramaria/androidfrasespensador/MainActivity.java
+++ app/src/main/java/com/ramaria/androidfrasespensador/MainActivity.java
@@ -3,12 +3,21 @@ package com.ramaria.androidfrasespensador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+        TextView text = findViewById(R.id.textResult); // Localizando o elemento e setando na variavel
+        text.setText("Test success");
+
     }
 }
